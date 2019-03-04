d41f82792fc368d617e73b3e55b9793189dd38c9
EssKayz <esskay@protonmail.com>
2019-03-04 14:01:54 +0200

    Ex 6.3, 6.4, 6.6, 6.7, 6.8

---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { voteFor } from '../reducers/anecdoteReducer'
+
+const Anecdotes = ({ store }) => {
+
+    const vote = (id) => {
+        console.log('vote', id)
+        store.dispatch(voteFor(id))
+    }
+
+    const list = store.getState().map(anecdote =>
+        <div key={anecdote.id}>
+            <div>
+                {anecdote.content}
+            </div>
+            <div>
+                has {anecdote.votes}
+                <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+        </div>
+    )
+
+    return (
+        list
+    )
+}
+
+export default Anecdotes
